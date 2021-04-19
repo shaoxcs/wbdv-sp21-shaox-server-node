a6218ec866c3248276cc42946284aa4dc92179e0
shaoxcs <shao.xue@outlook.com>
2021-04-18 19:25:45 -0700

    refactory services

--- services/questions/questions-service.js
+++ services/questions/questions-service.js
@@ -1,28 +1,11 @@
-const questions = require('./questions.json');
+const questionsDao = require("../daos/questions-dao")
 
-const createQuestion = () => {}
-const createQuestionForQuiz = () => {}
-
-const findAllQuestions = () => {
-  return  questions;
-}
-const findQuestionsForQuiz = (qzid) => {
-  return questions.filter((question) => {
-    return question.quizId === qzid;
-  })
-}
-
-const findQuestionById = (quid) => {
-  return questions.find((question) => {
-    return question._id === quid;
-  });
-}
-
-const updateQuestion = () => {}
-const deleteQuestion = () => {}
+const findAllQuestions = () => questionsDao.findAllQuestions();
+const findQuestionById = (qid) => questionsDao.findQuestionById(qid);
+const findQuestionsForQuiz = (qid) => questionsDao.findQuestionsForQuiz(qid);
 
 module.exports = {
-  createQuestion, findQuestionsForQuiz,
-  findAllQuestions, findQuestionById,
-  updateQuestion, deleteQuestion
-}
+  findAllQuestions,
+  findQuestionById,
+  findQuestionsForQuiz
+};
--- services/quizzes/quizzes-service.js
+++ services/quizzes/quizzes-service.js
@@ -1,25 +1,8 @@
-const quizzes = require('./quizzes.json')
+const quizzesDao = require('../daos/quizzes-dao');
 
-// TODO: Node.js Assignment this week
-const findAllQuizzes = () => {
-  return quizzes
-}
-const findQuizById = (quizId) => {
-  return quizzes.find((quiz) => {
-    return quiz._id === quizId
-  })
-}
-
-// console.log(findAllQuizzes())
-// console.log(findQuizById('123'))
-
-// TODO: MongoDB Assignment next week
-const createQuiz = () => {}
-const updateQuiz = () => {}
-const deleteQuiz = () => {}
-
+const findAllQuizzes = () => quizzesDao.findAllQuizzes();
+const findQuizById = (qid) => quizzesDao.findQuizById(qid);
 module.exports = {
-  createQuiz,
-  findQuizById, findAllQuizzes,
-  updateQuiz, deleteQuiz
-}
+  findAllQuizzes,
+  findQuizById
+};
